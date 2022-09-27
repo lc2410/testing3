64d82c98fbad1fd4f3ae832ac6a69d50e1d42dce
LiangChu870 <lc2410@g.rit.edu>
2022-09-27 14:55:25 -0400

    files are added to new-branch

---
+++ tk.py
@@ -0,0 +1,9 @@
+from tkinter import *
+window=Tk()
+# add widgets here
+
+window.title('Text App')
+window.geometry("300x300")
+l = Label(window, Text="Phone number")
+t = Text()
+window.mainloop(window)
